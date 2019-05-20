15c273cc855a913e54acee19e2bbece84e2eaee0
Tawan Yantapan <tawantawan1997@gmail.com>
2019-05-20 19:09:47 +0700

    create dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
     
   ]
 })
