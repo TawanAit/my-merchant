5d8f80188660c33e5e5e283050a037e07750bddd
Tawan Yantapan <tawantawan1997@gmail.com>
2019-05-24 12:36:51 +0700

    add Button Home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -8,6 +13,9 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatButtonModule,
   ]
 })
 export class HomeModule { }
