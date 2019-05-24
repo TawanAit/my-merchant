e1b2b7c685532ef967f8333ab3bf6de2fd574286
Tawan Yantapan <tawantawan1997@gmail.com>
2019-05-24 11:38:45 +0700

    add router home.component at app.module.ts & HomeComponent at home.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'home', component:HomeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
-import { LoginModule } from './login/login.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { RegisterComponent } from './register/register.component';
+import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
 
 
@@ -27,7 +23,8 @@ import { RegisterModule } from './register/register.module';
     
     LoginModule,
     DashboardModule,
-    RegisterModule
+    RegisterModule,
+    HomeModule,
     
   ],
   providers: [],
