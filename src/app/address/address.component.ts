4e2ea62099cdbee9d8e80bd2a3ddce2ca1999428
Tawan Yantapan <tawantawan1997@gmail.com>
2019-05-25 22:42:39 +0700

    link router

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-address',
@@ -7,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit() {
   }
 
+  logout(){
+    this.router.navigate(['/login']);
+  }
 }
 
 
