44c4ae76e498938e804352f094a0c7e3a9643717
Tawan Yantapan <tawantawan1997@gmail.com>
2019-06-03 15:37:12 +0700

    MatTableMOdule

--- src/app/manage-service/manage-service.module.ts
+++ src/app/manage-service/manage-service.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageServiceComponent } from './manage-service.component';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -8,7 +10,8 @@ import { ManageServiceComponent } from './manage-service.component';
   ],
   
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule
   ]
 })
 export class ManageServiceModule { }
